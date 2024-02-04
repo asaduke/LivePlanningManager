b871d02cee11c99f8d5041d2d9b45afffddafe6e
asaduke <kouricca1825.fish@gmail.com>
2024-02-05 06:37:19 +0900

    Add: lives・packing_itemsのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
   resources :password_resets, only: %i[new create edit update]
+  resources :lives do
+    resources :packing_items, only: %i[new create edit update destroy], shallow: true
+  end
 end
