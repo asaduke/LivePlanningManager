bfafbeaba7ccc5ccafaff61a24f4831523c9e3c7
asaduke <kouricca1825.fish@gmail.com>
2024-02-06 04:23:42 +0900

    Add: livesテーブルにuser_idを追加

--- app/models/live.rb
+++ app/models/live.rb
@@ -1,5 +1,6 @@
 class Live < ApplicationRecord
   has_many :packing_items, dependent: :destroy
+  belongs_to :user
 
   validates :title, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_03_181715) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_05_190721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_03_181715) do
     t.time "departure_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_lives_on_user_id"
   end
 
   create_table "packing_items", force: :cascade do |t|
@@ -50,5 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_03_181715) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
+  add_foreign_key "lives", "users"
   add_foreign_key "packing_items", "lives", column: "live_id"
 end
