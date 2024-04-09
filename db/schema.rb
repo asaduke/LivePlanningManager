e08972e9e1bd346ea2890ca6e6963092bb80212b
asaduke <kouricca1825.fish@gmail.com>
2024-04-10 00:26:53 +0900

    Add: usersとpostsのアソシエーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_09_145721) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_09_151756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,8 +45,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_09_145721) do
     t.bigint "packing_items_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["lives_id"], name: "index_posts_on_lives_id"
     t.index ["packing_items_id"], name: "index_posts_on_packing_items_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -77,5 +79,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_09_145721) do
   add_foreign_key "packing_items", "users"
   add_foreign_key "posts", "lives", column: "lives_id"
   add_foreign_key "posts", "packing_items", column: "packing_items_id"
+  add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
 end
