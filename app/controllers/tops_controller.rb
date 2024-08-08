0d7d7a14ed89b29eb8220926bfcc8e2367c1fc8b
asaduke <kouricca1825.fish@gmail.com>
2024-08-09 00:45:22 +0900

    Fix: ログイン時の最初のページをリスト一覧ページに変更しました

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,11 @@
 class TopsController < ApplicationController
   skip_before_action :require_login, only: :top
 
-  def top; end
+  def top
+    if current_user
+      redirect_to lives_path
+    else
+      render :top
+    end
+  end
 end
